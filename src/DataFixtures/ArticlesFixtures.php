93fab6e43c3f2397f8e2ac9e6f967b112852e917
dzeko003 <berenismassamba@gmail.com>
2023-07-27 10:23:56 +0100

    premier commit

---
+++ src/DataFixtures/ArticlesFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ArticlesFixtures extends Fixtures
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        for($i=1 ; $i <= 10 ;$i++){
+            $article = new Article();
+            $article -> setTitle("Titre de l\'article n-".$i)
+                     -> setContent("<p> Contenu de l'article n-".$i."</p>")
+                     ->setImage("http://placehold.it/350x150")
+                     ->setCreatedAt( new \DateTime());
+
+            $manager->persist($article);
+        }
+
+        $manager->flush();
+    }
+}
